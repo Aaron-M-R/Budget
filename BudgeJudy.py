2cb870c14fc1c697d1c577c602afa582ac48a0df
Aaron Rasin <aaronrasin@gmail.com>
2024-01-03 11:45:33 -0500

    added notebook comments

--- BudgeJudy.py
+++ BudgeJudy.py
@@ -3,34 +3,17 @@ import io
 import os
 import json
 import calendar
-import csv
-import openpyxl
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from openpyxl import Workbook, load_workbook
 from datetime import date, datetime
 from pathlib import Path
+
+import openpyxl
+from openpyxl import Workbook, load_workbook
+
+import csv
 from csv import DictReader
-
-
-
-
-#### Stored Data ####
-
-# Read in spending spreadsheet
-filepath = Path('Data') / 'PracticeCheckingSheet.xlsx'
-df = pd.read_excel(filepath)
-
-# Read in category descriptions of charges
-filepath = Path('Data') / 'category_descriptions.json'
-categories = json.loads(filepath.read_text())
-
-# Rename columns
-new_columns = dict(zip(df.columns.values, df.iloc[0]))
-df = df.rename(columns = new_columns).iloc[3:]
-
-
 
 
 #### Functions ####
@@ -57,14 +40,14 @@ def description_and_amount(Type, Description, Amount):
                     # return category, subcategory and charge description
                     return [category[0], subcategory[0], code]
 
-    return ["miscellaneous", "Miscellaneous", code]
+    return ["Miscellaneous", "Miscellaneous", code]
 
 # Combines withdrawal and deposit columns into one 'amount' column
 def amount(df):
-    if not math.isnan(df[4]):
-        return -df[4]
     if not math.isnan(df[5]):
-        return df[5]
+        return -df[5]
+    if not math.isnan(df[6]):
+        return df[6]
     else:
         return 0
 
@@ -79,7 +62,7 @@ def cat_plot(cats, df):
         new_df['Amount'] = new_df['Amount'].apply(lambda x: -x)
 
         # Create a pivot table for cumulative sum of amount by category and date
-        pivot_table = new_df.pivot_table(index='Date', columns='Category', values='Amount', aggfunc='sum').fillna(0)
+        pivot_table = new_df.pivot_table(index='Date', columns='Category', values='Amount', aggfunc='sum', sort=False).fillna(0)
         cumulative_df = pivot_table.cumsum()
 
         # Plotting
@@ -94,133 +77,117 @@ def cat_plot(cats, df):
         print("Input must be a list.")
 
 
+#### Loading Stored Data ####
 
+print("To begin, enter the name of your spreadsheet")
+sheet_name = input()
+if "." not in sheet_name:
+    sheet_name = f"{sheet_name}.xlsx"
 
-#### Create Spreadsheet ####
+# Read in spending spreadsheet
+sheetpath = Path('Data') / sheet_name
+df = pd.read_excel(sheetpath)
 
-# Accesses spreadsheet #
-working = True
-while working:
-    try:
-        print("What is the filepath for your sheet?")
-        filepath = input() + ".xlsx"
-        wb = openpyxl.load_workbook(filepath)
-        sheet = wb.active
-        working = False
-    except FileNotFoundError as e:
-        print("Sorry, that file could not be located. Please try again")
-
-original_sheet_name = "PracticeCheckingSheet.xlsx"
-wb = openpyxl.load_workbook(original_sheet_name)
-sheet = wb.active
-
-# Changes name of original sheet #
-print("Would you like to rename this sheet from {}? (Y/N))".format(wb.sheetnames[0]))
-rename_bool = input()
-if 'y' in rename_bool.lower():
-    print("Provide the new name or press enter to keep original name")
-    rename = input()
-    if len(rename)>0:
-        sheet.title=rename
+# Read in category descriptions of charges
+filepath = Path('Data') / 'category_descriptions.json'
+categories = json.loads(filepath.read_text())
 
 
 
 
-#### User enters start and end dates ####
+
+
+#### User Enters Start and End Dates ####
+
 startday, startmonth, startyear = 0, 0, 0
 endyear, endmonth, endday = 0, 0, 0
 
-# Start date info
-print("Enter the starting date for your budget:")
+# # Start date info
+# print("Enter the starting date for your budget:")
 
-# Collect starting year
-while startyear == 0:
-    print("Starting Year (YYYY)")
-    year = input().strip()
-    if len(year) == 2:
-        startyear = "20" + year
-    elif len(year) == 4:
-        startyear = year
-    else:
-        print("Sorry! Your date was not in (YYYY) format. Please try again.")
+# # Collect starting year
+# while startyear == 0:
+#     print("Starting Year (YYYY)")
+#     year = input().strip()
+#     if len(year) == 2:
+#         startyear = "20" + year
+#     elif len(year) == 4:
+#         startyear = year
+#     else:
+#         print("Sorry! Your date was not in (YYYY) format. Please try again.")
 
-# Collect starting month
-while startmonth==0:
-    print("Starting Month (MM)")
-    month = input().strip()
-    if len(month) == 1:
-        month = "0" + month
-    if 1<=int(month)<=12:
-        startmonth = month
+# # Collect starting month
+# while startmonth==0:
+#     print("Starting Month (MM)")
+#     month = input().strip()
+#     if len(month) == 1:
+#         month = "0" + month
+#     if 1<=int(month)<=12:
+#         startmonth = month
 
-    else:
-        print("Sorry! "+month+" isn\"t a valid month. Please try again.")
+#     else:
+#         print("Sorry! "+month+" isn\"t a valid month. Please try again.")
 
-# Collect starting day
-while startday == 0:
-    print("Starting Day (DD)")
-    day = input().strip()
-    if len(day) == 1:
-        day = "0" + day
-    if 1 <= int(day) <= 31:
-        startday = day
-    else:
-        print("Sorry! Your day is not in the range of the days in the month. Please try again.")
+# # Collect starting day
+# while startday == 0:
+#     print("Starting Day (DD)")
+#     day = input().strip()
+#     if len(day) == 1:
+#         day = "0" + day
+#     if 1 <= int(day) <= 31:
+#         startday = day
+#     else:
+#         print("Sorry! Your day is not in the range of the days in the month. Please try again.")
 
-# End date info
-print("Enter the ending date for your budget:")
-print("Do you want to see your spending up to the present? (Y/N)")
-answer = input()
-if "y" in answer.lower():
-    last_fiscal = str(date.today())
-else:
+# # End date info
+# print("Enter the ending date for your budget:")
+# print("Do you want to see your spending up to the present? (Y/N)")
+# answer = input()
+# if "y" in answer.lower():
+#     last_fiscal = str(date.today())
+# else:
 
-    # Collect ending year
-    while endyear == 0:
-        print("Final Year (YYYY)")
-        year = input().strip()
-        if len(year) == 2:
-            endyear = "20" + year
-        elif len(year) == 4:
-            endyear = year
-        else:
-            print("Sorry! Your date was not in (YYYY) format. Please try again.")
+#     # Collect ending year
+#     while endyear == 0:
+#         print("Final Year (YYYY)")
+#         year = input().strip()
+#         if len(year) == 2:
+#             endyear = "20" + year
+#         elif len(year) == 4:
+#             endyear = year
+#         else:
+#             print("Sorry! Your date was not in (YYYY) format. Please try again.")
 
-    # Collect ending month
-    while endmonth==0:
-        print("Final Month (MM)")
-        month = input().strip()
-        if len(month) == 1:
-            month = "0" + month
-        if 1<=int(month)<=12:
-            endmonth = month
-        else:
-            print("Sorry! "+month+" isn\"t a valid month. Please try again.")
+#     # Collect ending month
+#     while endmonth==0:
+#         print("Final Month (MM)")
+#         month = input().strip()
+#         if len(month) == 1:
+#             month = "0" + month
+#         if 1<=int(month)<=12:
+#             endmonth = month
+#         else:
+#             print("Sorry! "+month+" isn\"t a valid month. Please try again.")
 
-    # Collect ending day
-    while endday == 0:
-        print("Final Day (DD)")
-        day = input().strip()
-        if len(day) == 1:
-            day = "0" + day
-        if 1 <= int(day) <= 31:
-            endday = day
-        else:
-            print("Sorry! Your day is not in the range of the days in the month. Please try again.")
+#     # Collect ending day
+#     while endday == 0:
+#         print("Final Day (DD)")
+#         day = input().strip()
+#         if len(day) == 1:
+#             day = "0" + day
+#         if 1 <= int(day) <= 31:
+#             endday = day
+#         else:
+#             print("Sorry! Your day is not in the range of the days in the month. Please try again.")
 
-    last_fiscal = "{}-{}-{}".format(endyear, endmonth, endday)
-first_fiscal = "{}-{}-{}".format(startyear, startmonth, startday)
+    # last_fiscal = "{}-{}-{}".format(endyear, endmonth, endday)
+# first_fiscal = "{}-{}-{}".format(startyear, startmonth, startday)
+
+first_fiscal = "2023-01-01"
+last_fiscal = "2023-12-31"
 
 # The range of dates selected by the user
 budget_days = pd.date_range(start=first_fiscal, end=last_fiscal)
-
-# Makes a list of the months in budget_days 
-budget_months = []
-for day in budget_days:
-    if str(day)[5:7] not in budget_months:
-        budget_months.append(str(day)[5:7])
-
-
 
 
 #### Spreadsheet transformations ####
@@ -251,32 +218,73 @@ df = df.assign(Year=df['Date'].apply(lambda x: int(str(x)[:4])),
 # Create spending table grouped by month
 totals = df.drop(columns=['Date', 'Code', 'RunningBalance', 'Day'])
 totals = totals.groupby(['Year', 'Month', 'Category', 'Subcategory']).sum()
-totals = totals.reset_index(level=2, drop=True).reset_index(level=1, drop=False)
+totals = totals.reset_index(level=1, drop=False)
 totals = totals.assign(Month=totals['Month'].apply(lambda x: calendar.month_name[x]))
-totals = totals.pivot(columns='Month').fillna(0)
+totals = totals.pivot_table(index=['Category', 'Subcategory'], columns='Month', values='Amount', aggfunc='sum', sort=False).fillna(0)
 totals = totals.assign(Total=totals.sum(axis=1))
 
-# Export DataFrame to Excel
-print("What would you like to call your new sheet?")
-newsheet_name = input()
-wb.create_sheet(newsheet_name)
-wb.save(original_sheet_name)
-newsheet = wb[newsheet_name]
-totals.to_excel()
+# Creat a dictionary for each category
+list_of_dicts = [{item[0].capitalize(): list(item[1].keys())} for item in categories.items()]
 
-# Create an ExcelWriter object
-with pd.ExcelWriter(filepath, engine='xlsxwriter') as writer:
+# Extract keys and values from the list of dictionaries
+keys = [list(d.keys())[0] for d in list_of_dicts]
+values = [list(d.values())[0] for d in list_of_dicts]
 
-    # Write the DataFrame to a sheet named 'Sheet1'
+# Create a MultiIndex from the keys and values
+multi_index = pd.MultiIndex.from_tuples([(key, val) for key, val_list in zip(keys, values) for val in val_list], names=['Category', 'Subcategory'])
+
+# Create an empty DataFrame and fill it with values from totals
+empty_df = pd.DataFrame(index=multi_index, columns=[])
+totals = empty_df.combine_first(totals).fillna(0)
+
+
+
+
+#### Create Spreadsheet ####
+
+# Changes name of original sheet #
+# print("Would you like to rename this sheet from {}? (Y/N))".format(wb.sheetnames[0]))
+# answer = input()
+# if 'y' in answer.lower():
+    # print("Provide the new name or press enter to keep original name")
+    # rename = input()
+    # if len(rename)>0:
+    #     sheet.title=rename
+
+# Load the existing Excel workbook
+workbook = openpyxl.load_workbook(sheetpath)
+
+# Get the old sheet
+old_sheet = workbook[workbook.sheetnames[0]]
+
+# Rename the old sheet to the new name
+old_sheet.title = 'original'
+
+# Save the changes to the workbook
+workbook.save(sheetpath)
+
+
+
+
+# Name new excel sheet according to user input
+# print("What would you like to call your new sheet?")
+newsheet_name = '2023'
+
+# Open the existing Excel file
+with pd.ExcelWriter(sheetpath, engine='openpyxl', mode='a', if_sheet_exists='new') as writer:
+
+    # Write the DataFrame to a new sheet
     totals.to_excel(writer, sheet_name=newsheet_name, index=True)
+
 
 
 
 
 #### Plotting ####
 
-# Bar Plot #
-plot_df = totals.copy()[['Total']]
+# Category Bar Plot #
+plot_df = totals.copy()[['Total']].reset_index(level=1, drop=True)
+plot_df = plot_df.reset_index().groupby('Category').sum()
 plot_df['Total'] = plot_df['Total'].apply(lambda x: -x)
 plot_df.plot(kind='bar', legend=None, figsize=(10, 6))
 plt.title('Total Amount Spent on Each Category')
@@ -285,15 +293,32 @@ plt.ylabel('Total Amount')
 plt.xticks(rotation=45, ha='right')
 plt.show()
 
+# Subcategory Bar Plot #
+subplot_df = totals.copy()[['Total']].reset_index(level=0, drop=True)
+subplot_df['Total'] = subplot_df['Total'].apply(lambda x: -x)
+subplot_df.plot(kind='bar', legend=None, figsize=(10, 6))
+plt.title('Total Amount Spent on Each Subcategory')
+plt.xlabel('Category')
+plt.ylabel('Total Amount')
+plt.xticks(rotation=45, ha='right')
+plt.show()
+
 # Line Plot #
 print("Would you like to visualize your spending over time? (Y/N)")
-vis = input()
-if 'y' in vis.lower():
-    visualize = True
-    while visdone:
-        print("Enter the categories you want to see.")
-        vistype = input().strip('[]')
-        viscats = [word.strip() for word in vistype.split(',')]
+answer = input()
+if 'y' in answer.lower():
+    plotting = True
+    while plotting:
+        print("Enter the categories you want to see. Answer \'all\' to see every category")
+        vistype = input()
+        if vistype == 'all':
+            viscats = list(df['Category'].unique())
+        else:
+            viscats = [word.strip() for word in vistype.strip('[]').split(',')]
         cat_plot(viscats, df)
+        print("Would you like to plot again? (Y/N)")
+        answer = input()
+        if 'y' not in answer.lower():
+            plotting = False
 
 
